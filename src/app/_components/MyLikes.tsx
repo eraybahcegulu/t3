e4b3619357f83e10104e9f8b9afe58da6f0de011
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-23 19:55:14 +0300

    Create MyLikes.tsx

---
+++ src/app/_components/MyLikes.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const MyLikes = () => {
+  return (
+    <Link href={"/likes"} className="m-2 rounded-full cursor-pointer bg-white/10 px-4 py-3 font-semibold no-underline transition hover:bg-white/20"> My Likes </Link>
+  )
+}
+
+export default MyLikes
